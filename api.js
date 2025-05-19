5509f87bb1c07ba610c779a8ffc59c57a3f61240
nayeemjannat <naimsstore@gmail.com>
2025-05-19 21:31:11 +0600

    call from api

---
+++ api.js
@@ -0,0 +1,45 @@
+const loadCourse=async()=>{
+    const res= await fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+    const data= await res.json()
+    const courses=data.data.tools;
+    // console.log(courses)
+const courseContainer=document.getElementById('course-container');
+courseContainer.innerHTML=''
+courseContainer.className='flex flex-wrap justify-center gap-6 mt-6 px-2 md:px-4'
+    //display data
+courses.forEach(course=>
+    {
+        console.log(course);
+        //create a div
+        const courseCard=document.createElement('div')
+        courseCard.className=`card bg-base-100 w-96 shadow-sm`
+        
+//create innerHtml
+        courseCard.innerHTML=
+`
+  <figure class="px-10 pt-10">
+    <img src="${course.image}"
+      alt="course"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-start text-center">
+    <h2 class="card-title">Features</h2>
+    <ol class="text-left list-inside list-decimal">
+ ${course.features.map(feature=>`<li>${feature}</li>`).join('')}
+    </ol>
+    <p><li>${course.features}</li></p>
+    <hr class="1px solid to-black">
+    <p class="text-bold">${course.name}</p>
+     <p>${course.published_in}</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+  `
+//append child
+courseContainer.appendChild(courseCard)
+
+    });
+}
+
+loadCourse();
